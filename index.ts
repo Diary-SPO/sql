27c0311ccfe81adc420d3fd7c4797f30a6149f75
Semyon Okulov <khidey@inbox.ru>
2023-11-18 12:56:49 +0700

    fix imports

--- index.ts
+++ index.ts
@@ -1,5 +1,8 @@
-export * from './src/types'
-export * from './src/utils'
-export * from './src/createQueryBuilder'
-export * from './src/fetcher'
-export * from './src/crypto'
+import createQueryBuilder from './src/createQueryBuilder'
+import { QueryBuilder, HTTPMethods, ApiResponse } from './src/types'
+import { fetcher } from './src/fetcher'
+import { encrypt, decrypt } from './src/crypto'
+
+export { fetcher, decrypt, encrypt }
+export type { QueryBuilder, HTTPMethods, ApiResponse }
+export default createQueryBuilder
